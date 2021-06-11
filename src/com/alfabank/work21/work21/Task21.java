611087d069e44432bb239305b4baab23c1869eb1
Svetlana1605 <85413486+Svetlana1605@users.noreply.github.com>
2021-06-12 00:49:39 +0300

    Initial commit

---
+++ src/com/alfabank/work21/work21/Task21.java
@@ -0,0 +1,8 @@
+package com.alfabank.work21.work21;
+
+public class Task21 {
+
+    public static void main(String[] args) {
+        System.out.println("Time = " + 3 * 7 * 24 * 60 * 60);
+    }
+}
---
+++ src/com/alfabank/work21/work24/Task24.java
@@ -0,0 +1,16 @@
+
+    package com.alfabank.work21.work24;
+
+    public class Task24 {
+        public static void main(String[] args) {
+            int number = -753;
+            int digit_1 = number % 10;
+            int digit_2 = number / 10 % 10;
+            int digit_3 = number / 100;
+
+            int number1 = digit_1 * 100 + digit_2 * 10 + digit_3;
+
+            System.out.println(number1);
+            System.out.println(number - number1);
+        }
+    }
